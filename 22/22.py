75a780f5445e6f2bff8dea4aafdda7fd25d09078
lirakH <135222007+lirakH@users.noreply.github.com>
2025-06-18 17:50:25 +0200

    Update 22.py

---
+++ 22/22.py
@@ -0,0 +1,23 @@
+"""
+Pydantic 
+
+Pydantic is a powerful data validation and settings management library for Python that leverages Python's type annotations.
+
+Key Features:
+  Data Validation: Ensures that the input data conforms to the expected types. 
+  Type Coercion: Automatically converts compatible types (e.g., strings to integers). 
+  Nested Models: Supports defining nested data structures. 
+  Error Reporting: Provides detailed error messages when validation fails. 
+  Settings Management: Simplifies configuration management by loading settings from environment variables and other sources. 
+
+Pip install pydantic 
+
+Pydantic dependencies:
+  ● pydantic-core: Core validation logic for pydantic written in rust. 
+  ● typing-extensions: Backport of the standard library typing module. 
+  ● annotated-types: Reusable constraint types to use with typing.Annotated. 
+
+pip install pydantic[email] 
+
+Core Concepts: Models, Field Types & Validation, Default Values and Optional Fields, Field Constraints 
+"""

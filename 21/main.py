08338a817e7a59ee42d3b1d814661b6121ec85e0
lirakH <135222007+lirakH@users.noreply.github.com>
2025-06-18 17:07:52 +0200

    Update main.py

--- 21/main.py
+++ 21/main.py
@@ -29,9 +29,29 @@ def read_item(item_id: int):
 def read_items():
     return {"users": ["users1", "users2", "users3"]}
 
+# Path Parameters 
 @app.get("/users/{users_id}")
 def read_item(users_id: int):
     return {"users_id": users_id}
 
+# Query Parameters
+@app.get("/items/")
+    # skip and limit
+def get_items(skip: int=0, limit: int=10):
+    return {"skip": skip, "limit": limit}
+    #/items/?skip=3&limit=10
 
+""" Post Request """
+@app.post("/items/")
+def create_item(name: str, price: float):
+    return {"item_name": name, "item_price": price}
 
+""" Put Request """
+@app.put("/items/{item_id}")
+def update_item(item_id: int, name: str, price: float):
+    return {"item_id": item_id, "item_name": name, "item_price": price}
+
+""" Delete Request """
+@app.delete("/items/{item_id}")
+def delete_item(item_id: int):
+    return {"message": f"Item {item_id} deleted"}

7c2c4fa6760d09b3d5434054c52f57b7d776b055
lirakH <135222007+lirakH@users.noreply.github.com>
2025-06-27 17:49:40 +0200

    Update 23.py

---
+++ 23/23.py
@@ -0,0 +1,14 @@
+"""
+Advanced Type Annotations and Integrating Tools 
+
+Advanced type annotations in Python provide a strong way to specify the expected types of variables, function parameters, and return values.
+
+They enhance code readability and maintainability, especially in larger codebases, by making the data flow and function contracts explicit.
+
+Some key advanced type annotations include Optional, Union, and Any.
+
+The `Optional` type is used when a variable or function parameter can be of a specified type or `None`.
+The Union type is used to indicate that a variable or parameter can be one of several specified types.
+The Any type is used when a variable can be of any type.
+
+"""

b9de6dfb8156300fc61669f05ea25ae6af255916
lirakH <135222007+lirakH@users.noreply.github.com>
2025-06-30 17:51:00 +0200

    Update 23.py

--- 23/23.py
+++ 23/23.py
@@ -12,3 +12,47 @@ The Union type is used to indicate that a variable or parameter can be one of se
 The Any type is used when a variable can be of any type.
 
 """
+from typing import Optional, Any, Union, List
+
+def get_name(name: Optional[str] = None) -> str:
+  if name:
+    return name
+  return "Anonymous"
+
+print(get_name())
+print(get_name("Lirak"))
+print(get_name(12))
+
+def process_value(value: Union[int, str]) -> str:
+  if isinstance(value, int):
+    return f"Number: {value}"
+  return f"String: {value}"
+
+# print(process_value())
+print(process_value("Lirak"))
+print(process_value(12))
+
+def process_anything(value: Any) -> str:
+  tipi = type(value)
+  return f"Processed {type}: {value}"
+
+# print(process_anything())
+print(process_anything("Lirak"))
+print(process_anything(12))
+print(process_anything([12,3,"a",9]))
+
+def sum_of_numbers(numbers: List[int]) -> int:
+  return sum(numbers)
+
+numbers: List[int] = [1,2,3]
+result: int = sum_of_numbers(numbers)
+print(result)
+
+
+
+
+
+
+
+
+

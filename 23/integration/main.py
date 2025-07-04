2dcf4cbe84da15e59395d8eea7f7dbd011b74d95
lirakH <135222007+lirakH@users.noreply.github.com>
2025-07-04 17:49:14 +0200

    Update main.py

---
+++ 23/integration/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from models import Developer, Project
+
+app = FastAPI()
+
+@app.post("/developers/")
+def create_developer(developer: Developer):
+  return {"message": "Developer creadet successfuly", "developer": developer}
+
+@app.post("/projects/")
+def create_developer(project: Project):
+  return {"message": "Project creadet successfuly", "project": project}
+
+@app.get("/projects/")
+def get_projects():
+  #This is placeholder; in a real application, you'd fetch this data from a database
+  sample_project = Project(
+    title = "Sample",
+    description = "This is a sample project",
+    languages = ["Python" "JavaScript", "HTML"],
+    lead_deeloper = Developer(name="John Doe", experience=5)
+  )
+  return {"projects": [sample_project]}

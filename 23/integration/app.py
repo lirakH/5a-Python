0d5fe6fdc77d27e6086707f9c385bd6126537ea9
lirakH <135222007+lirakH@users.noreply.github.com>
2025-07-07 17:27:53 +0200

    Update app.py

--- 23/integration/app.py
+++ 23/integration/app.py
@@ -13,3 +13,23 @@ if st.button("Create Developer"):
     dev_data = {"name": dev_name, "experience": dev_experience}
     response = requests.post("http://localhost:8000/developers/", json=dev_data)
     st.json(response.json())
+
+# Project Form
+st.header("Add a Project")
+proj_title = st.text_input("Project Title")
+proj_desc = st.text_area("Project Description")
+proj_langs = st.text_input("Languages Used (Comma-separated)")
+lead_dev_name = st.text_input("Lead Developer Name")
+lead_dev_exp = st.number_input("Lead Developer Experience (Years)", min_value=0, max_value=50, value=0)
+
+if st.button("Create Project"):
+    lead_dev_data = {"name": lead_dev_name, "experience": lead_dev_exp}
+    proj_data = {
+        "title": proj_title,
+        "description": proj_desc,
+        "languages": proj_langs.split(","),
+        "lead_developer": lead_dev_data
+    }
+    response = requests.post("http://localhost:8000/projects/", json=proj_data)
+    st.json(response.json())
+

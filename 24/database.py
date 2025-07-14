905f6ab33a3d0fcb68e2cc00825a34bb4fe2e0ef
lirakH <135222007+lirakH@users.noreply.github.com>
2025-07-14 17:28:09 +0200

    Create database.py

---
+++ 24/database.py
@@ -0,0 +1,5 @@
+#The sqlite3 library in Python provides a simple and efficient way to interact with SQLite databases.
+import sqlite3
+
+#conect our file/app with database (or create db if it doesnt exist)
+conection = sqlite3.connect('mydatabase.db');

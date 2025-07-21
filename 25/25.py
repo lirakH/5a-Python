b9a402958720c77677deafa824c25e6669481949
lirakH <135222007+lirakH@users.noreply.github.com>
2025-07-21 18:05:13 +0200

    Update 25.py

--- 25/25.py
+++ 25/25.py
@@ -17,3 +17,29 @@ def read_root():
 @app.post("/movies/", response_model=Movie)
 def create_movie(movie: MovieCreate):
   movie_id = database.create_movie(movie)
+  return models.Movie(id=movie_id, **movie.dict())
+
+@app.get("/movies/", response_model=List[Movie])
+def read_all_movies():
+  return database.read_movies()
+
+@app.get("/movies/{movie_id}", response_model=Movie)
+def read_movie(movie_id: int):
+  movie = database.read_movie(movie_id)
+  if movie is None:
+    raise HTTPException(status_code=404, detatil="Movie not found!")
+  return movie
+
+@app.put("/movies/{movie_id}", response_model=Movie)
+def update_movie(movie_id: int, movie: MovieCreate):
+  updated = database.update_movie(movie_id, movie)
+  if not updated:
+    raise HTTPException(status_code=404, detatil="Movie not found!")
+  return models.Movie(id=movie_id, **movie.dict())
+
+@app.delete("/movies/{movie_id}", response_model=dict)
+def delete_movie(movie_id: int):
+  deleted = database.delete_movie(movie_id)
+  if not deleted:
+    raise HTTPException(status_code=404, detatil="Movie not found!")
+  return {"message": "Movie deleted successfully!"}

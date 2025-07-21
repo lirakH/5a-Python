bca623dfe4ed2c01ddad401a91376f6ae06f818c
lirakH <135222007+lirakH@users.noreply.github.com>
2025-07-21 17:31:23 +0200

    Update 25.py

--- 25/25.py
+++ 25/25.py
@@ -12,3 +12,8 @@ app = FastAPI()
 @app.get("/")
 def read_root():
   return {"message": "Welcome to our Movie CRUD API"}
+
+#CRUD(api comands) - Create(post) Read(get) Update(put) Delete(delete)
+@app.post("/movies/", response_model=Movie)
+def create_movie(movie: MovieCreate):
+  movie_id = database.create_movie(movie)
